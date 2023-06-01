b92a219024c4f5c8275cdc5ec701e5638e1f6629
oriavsapir <85383966+oriavsapir@users.noreply.github.com>
2023-06-01 14:19:57 +0300

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,66 +1,61 @@
-board = [[5, 1, 7, 6, 0, 0, 0, 3, 4],
-         [2, 8, 9, 0, 0, 4, 0, 0, 0],
-         [3, 4, 6, 2, 0, 5, 0, 9, 0],
-         [6, 0, 2, 0, 0, 0, 0, 1, 0],
-         [0, 3, 8, 0, 0, 6, 0, 4, 7],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 9, 0, 0, 0, 0, 0, 7, 8],
-         [7, 0, 3, 4, 0, 0, 5, 6, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+board = [
+    [0, 1, 0, 0, 0, 6, 4, 0, 0],
+    [0, 9, 0, 4, 5, 0, 0, 2, 0],
+    [5, 0, 0, 0, 0, 7, 0, 0, 0],
+    [0, 0, 0, 0, 0, 5, 0, 0, 0],
+    [9, 0, 0, 1, 8, 0, 7, 0, 0],
+    [0, 2, 0, 0, 0, 0, 0, 0, 3],
+    [8, 0, 0, 9, 4, 0, 1, 0, 0],
+    [0, 0, 0, 6, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 7, 0]
+]
 
 
 def print_board(board):
-    for i in range(9):
-        for j in range(9):
-            print(board[i][j], end=" ")
-        print()
+    for row in board:
+        print(*row)
 
 
-def validator(row, col, matrix):
-    valid = 0
-    for number in range(1, 10):
-        # Check if the number is not in the col/row/matrix[3*3].
-        if number not in row and number not in col and number not in matrix:
-            valid_number = number
-            valid += 1
-    # Check if there is only one number that can match.
-    if valid == 1:
-        return valid_number
-    else:
+def is_valid(board, row, col, num):
+    # Check if the number is not present in the same row
+    if num in board[row]:
         return False
 
+    # Check if the number is not present in the same column
+    for i in range(9):
+        if board[i][col] == num:
+            return False
 
-def start(board):
-    count = -1
-    for row in board:
-        for col in row:
-            count += 1
-            if col == 0:
-                col_list = []
-                matrix = []
-                current_row = int(count / len(row))
-                current_col = int(count % 9)
-                strat_row = current_row - (current_row % 3)
-                strat_col = current_col - (current_col % 3)
-                for x in range(3):
-                    for y in range(3):
-                        matrix.append(board[strat_row + x][strat_col + y])
-                for i in range(len(row)):
-                    col_list.append(board[i][current_col])
-                check = validator(row, col_list, matrix)
-                if check:
-                    board[current_row][current_col] = check
-    return board
+    # Check if the number is not present in the same 3x3 box
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
 
 
-start(board)
-print("---- This is first try -----")
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        else:
+                            board[row][col] = 0
+                return False
+    return True
+
+
+print("---- Original Board ----")
 print_board(board)
-tried = 1
 
-# Check if the sudoku is solved.
-while any(0 in row for row in board):
-    start(board)
-    tried += 1
-    print("---- This is try number", tried, "-----")
-    print_board(board)
+solve_sudoku(board)
+
+print("---- Solved Board ----")
+print_board(board)
